f1feaaf3830d4f38eb0a0da81936fb9b66a0a09f
aliharriss <aliharriss1995@gmail.com>
2021-07-13 03:20:34 +1200

    streaming done

--- README.md
+++ README.md
@@ -4,10 +4,23 @@ This project contains
 - authentication/oauth2 - GoogleAuth
 - redux forms - StreamCreate
 - form validation - StreamCreate
-- json-server mock api - api
 - async action creators - actions/index
 - react-router-dom - throughout
 - reacting from action creator - history, App, actions/index
 - routing with dynamic url (params) - StreamList, App, StreamEdit
 - reusing components - StreamCreate, StreamEdit, StreamForm
 - portals - StreamDelete, Modal: render the element as a child of another element so it can overlay other stuff
+- json-server mock api - api
+- rtmp server - rtmp-server/index (node-media-server)
+
+# To use
+- ```cd react-streaming && npm install && npm start```
+- ```cd ../api && npm install && npm start```
+- ```cd ../rtmp-server && npm install && npm start```
+- Get OBS studio
+  - Send the stream to url: ```rtmp://localhost:1935/live``` with a key: ```1```
+  - start streaming
+- Go to ```http://localhost:3000``` in the browser
+- Log in with google
+- Create a stream
+- View the stream
--- react-streaming/src/components/streams/StreamShow.js
+++ react-streaming/src/components/streams/StreamShow.js
@@ -1,27 +1,65 @@
-import React, { useEffect } from "react";
+import React, { Component, useEffect, useRef } from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { getStream } from "../../actions";
 
-const StreamShow = (props) => {
-  useEffect(() => {
-    props.getStream(props.match.params.id);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+    // get a ref to the video
+    this.videoRef = React.createRef();
+  }
 
-  const renderStream = () => {
-    if (props.stream) {
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    this.props.getStream(id);
+
+    // try to handle streaming video on first render
+    this.buildPlayer()
+
+    return
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }
+
+  componentDidUpdate(){
+    // try to handle streaming video on first each update
+    this.buildPlayer();
+  }
+
+  componentWillUnmount(){
+    this.player.destroy()
+  }
+
+  buildPlayer(){
+    const id = this.props.match.params.id;
+    if(this.player||!this.props.stream){
+      return
+    }
+      this.player = flv.createPlayer({
+        type: "flv",
+        url: `http://localhost:8000/live/${id}.flv`,
+      });
+      this.player.attachMediaElement(this.videoRef.current);
+      this.player.load();
+  }
+
+  renderStream() {
+    if (this.props.stream) {
       return (
         <div>
-          <h1>{props.stream.title}</h1>
-          <h5>{props.stream.description}</h5>
+          <video ref={this.videoRef} style={{ width: "100%" }} controls></video>
+          <h1>{this.props.stream.title}</h1>
+          <h5>{this.props.stream.description}</h5>
         </div>
       );
     }
     return <div>Loading...</div>;
-  };
+  }
 
-  return <div>{renderStream()}</div>;
-};
+  render() {
+    return <div>{this.renderStream()}</div>;
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   return {
